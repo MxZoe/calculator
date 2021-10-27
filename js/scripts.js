8e301ee03c3a78b9ff35b61e679c9c62e2b123e5
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-26 20:02:56 -0600

    add alert to display the function results.

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,10 @@ function divide(number1, number2){
 
 const num1 = parseInt(prompt("Enter a number: "));
 const num2 = parseInt(prompt("Enter a second number: "));
-const result = add(num1, num2);
+const resultAdd = add(num1, num2);
+const resultSub = subtract(num1,num2);
+const resultMult = multiply(num1,num2);
+const resultDiv = divide(num1, num2);
 
-alert(result);
+alert("addition: " + resultAdd + ", " + "subtraction: " + resultSub + ", " +
+"multiplication: " + resultMult + ", " + "division: " + resultDiv + ".");
