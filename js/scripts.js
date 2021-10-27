58b5d0acf5edd71a884b31b316b2694ed3aa33fb
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-26 19:57:23 -0600

    add subtract function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ function add(number1, number2){
   return number1 + number2;
 }
 
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+
 const num1 = parseInt(prompt("Enter a number: "));
 const num2 = parseInt(prompt("Enter a second number: "));
 const result = add(num1, num2);
